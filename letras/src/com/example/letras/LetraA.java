98ab7ae31a2bca5d116c1117ae3712c383737119
Jonathan López <jlopez@fipasoft.com.mx>
2014-10-29 10:03:57 -0600

    Ejemplo de reproducción de video

---
+++ letras/src/com/example/letras/LetraA.java
@@ -0,0 +1,57 @@
+package com.example.letras;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class LetraA extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+	    //Remove notification bar
+	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.activity_letraa);
+		
+		MediaController media=new MediaController(this);
+		VideoView video=(VideoView) findViewById(R.id.videoView1);
+		video.setMediaController(media);
+		Intent intent=getIntent();
+		String letra  = intent.getStringExtra("id");
+		Uri path=Uri.parse("android.resource://com.example.letras/raw/"+letra);
+		video.setVideoURI(path);
+		video.requestFocus();
+		video.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.letra, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
